72bb866b6cc9f180707cb7a8416ac73179785150
Kelum <kelum.13@cse.mrt.ac.lk>
2015-12-12 12:22:03 +0530

    register files

--- src/TaxiBundle/Controller/BlogController.php
+++ src/TaxiBundle/Controller/BlogController.php
@@ -7,8 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class BlogController extends Controller {
-  public function listAction() {
-	  return new Response("Welcome to Blog List");
+  public function registerAction() {
+	      return $this->render('TaxiBundle:Blog:register.html.twig');
+
   }
 }
 
